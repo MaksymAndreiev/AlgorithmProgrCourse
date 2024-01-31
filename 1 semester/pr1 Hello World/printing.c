3f0ada62e4a54ae88396ebc35c6d896bd398241d
Maksym Andreiev <maxoggyha@gmail.com>
2024-02-01 00:39:25 +0900

    Add README.md

---
+++ 1 semester/pr1 Hello World/printing.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    printf("**********\n");
+    for (int i = 0; i < 8; i++) {
+        printf("*        *\n");
+    }
+    printf("**********\n");
+
+    printf("    ****\n");
+    printf("  *    *\n");
+    printf(" *      *\n");
+    printf("  *    *\n");
+    printf("    ****\n");
+
+    printf("    *\n");
+    printf("   ***\n");
+    printf("  *****\n");
+    printf("    *\n");
+    printf("    *\n");
+    printf("    *\n");
+
+    printf("      *\n");
+    printf("    *   *\n");
+    printf("  *       *\n");
+    printf("    *   *\n");
+    printf("      *\n");
+
+    return 0;
+}
